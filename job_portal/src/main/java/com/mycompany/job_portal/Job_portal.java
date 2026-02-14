b95345868975d6ee882f897abaa2f55e1ffc5851
YXing717 <gunyx-wp22@student.tarc.edu.my>
2026-02-15 02:45:59 +0800

    Initial setup

---
+++ job_portal/src/main/java/com/mycompany/job_portal/Job_portal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.job_portal;
+
+/**
+ *
+ * @author GUN YU XING
+ */
+public class Job_portal {
+
+    public static void main(String[] args) {
+        System.out.println("This is a test file.");
+    }
+}
