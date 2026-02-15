1746cd169283e4fb358baa2d8a6d3d42e09af5a7
YXing717 <gunyx-wp22@student.tarc.edu.my>
2026-02-15 23:37:06 +0800

    JP-7: Update for Jira sync test

---
+++ JobPortal/src/main/java/com/mycompany/jobportal/JobPortal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jobportal;
+
+/**
+ *
+ * @author GUN YU XING
+ */
+public class JobPortal {
+
+    public static void main(String[] args) {
+        System.out.println("Test commit again using JP-7: Update for Jira sync test");
+    }
+}
