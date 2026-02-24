485ddb537c7066273b18ca61fc9b1a8dd5b842cf
dan09 <danthecoder77@gmail.com>
2026-02-24 10:15:53 +0800

    JP-6 added job post to store job information

--- JobPortal/src/main/java/com/mycompany/DanielTayYiFung/JobPost.java
+++ JobPortal/src/main/java/com/mycompany/DanielTayYiFung/JobPost.java
@@ -1,1 +1,68 @@
+package com.mycompany.job_portal;
 
+public class JobPost {
+
+    private String jobTitle;
+    private String jobCompany;
+    private String jobDescription;
+    private String jobLocation;
+    private double jobSalary;
+
+    public JobPost(String jobTitle, String jobCompany, String jobDescription, String jobLocation, double jobSalary) {
+        this.jobTitle = jobTitle;
+        this.jobCompany = jobCompany;
+        this.jobDescription = jobDescription;
+        this.jobLocation = jobLocation;
+        this.jobSalary = jobSalary;
+    }
+
+    // setter & getter
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobCompany(String jobCompany) {
+        this.jobCompany = jobCompany;
+    }
+
+    public String getJobCompany() {
+        return jobCompany;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobSalary(double jobSalary) {
+        this.jobSalary = jobSalary;
+    }
+
+    public double getJobSalary() {
+        return jobSalary;
+    }
+
+    @Override
+    public String toString() {
+        return jobTitle + " - " + jobCompany;
+    }
+
+    public String toFile() {
+        return jobTitle + " | " + jobCompany + " | " + jobLocation + " | " + jobDescription + " | " + jobSalary;
+    }
+}
