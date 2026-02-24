95ac94f303ba1b113201d27f8b2d6cc51b9db207
dan09 <danthecoder77@gmail.com>
2026-02-24 10:29:23 +0800

    JP-6 fix code alignment

--- JobPortal/src/main/java/com/mycompany/DanielTayYiFung/JobPortal.java
+++ JobPortal/src/main/java/com/mycompany/DanielTayYiFung/JobPortal.java
@@ -1,3 +1,5 @@
+package com.mycompany.job_portal;
+
 public class JobPortal extends JFrame{
   private JComboBox<JobPost> jobList;
   private JTextField jobTitleField;
@@ -114,7 +116,7 @@ public class JobPortal extends JFrame{
         }
     }
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new JobPortal().setVisible(true));
     }
 }
